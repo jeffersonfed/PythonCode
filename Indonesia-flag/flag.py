18a6473006140baff4ab255b9cc19862e172bace
Jefferson <stupidjeff430@gmail.com>
2023-08-17 13:15:28 +0900

    Update

--- Indonesia-flag/flag.py
+++ Indonesia-flag/flag.py
@@ -5,7 +5,6 @@ import matplotlib.patches as fig
 u = patch.Rectangle((0,1), width=6, height=2, facecolor="#FFFFFF", edgecolor="black")
 d = patch.Rectangle((0,3), width=6, height=2, facecolor="#ff0000", edgecolor="black")
 
-
 # for three flags example
 
 # d = patch.Rectangle((0,1), width=10, height=2, facecolor="#ff0000", edgecolor="black")
